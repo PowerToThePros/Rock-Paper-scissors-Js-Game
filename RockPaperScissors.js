d28befff5aa8380ad1793421a0d1423c346d9467
ForcepowerInteractive <s_hillenbrand@t-online.de>
2026-03-13 20:12:33 +0100

    added: event listeners insted of onClick

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -10,7 +10,47 @@
 
       updateResultElement('Make a move');
       updateScoreElement();
-      
+
+      let isAutoPlaying = false;
+
+      let modal = document.querySelector('.js-modal');
+
+      // event listeners insted of onclick
+                                                                          // common mistake is that you put only playGame('rock') in here but that would call a function
+      document.querySelector('.js-rock-button').addEventListener('click', () => {playGame('rock')});
+      document.querySelector('.js-paper-button').addEventListener('click', () => {playGame('paper')});
+      document.querySelector('.js-scissors-button').addEventListener('click', () => {playGame('scissors')});
+      document.querySelector('.js-auto-play-button').addEventListener('click', () => {autoPlay();});
+      document.querySelector('.js-reset-score-button').addEventListener('click', () => {resetScore();});
+
+      // add event listener has a parameter that contains the event
+      document.body.addEventListener('keydown', (event)=>{
+
+        if(modal.classList.contains('visible')) return;
+
+        if(event.key === 'r')
+        {
+          playGame('rock');
+        }
+        else if(event.key === 'p')
+        {
+          playGame('paper');
+        }
+        else if(event.key === 's')
+        {
+          playGame('scissors');
+        }
+        else if(event.key === 'a')
+        {
+          autoPlay();
+        }
+        else if(event.key === 'Backspace')
+        {
+          resetScore();
+        }
+
+      });
+
       function playGame(move)
       {
         const computerMove = pickComputerMove();
@@ -120,4 +160,60 @@
       function updateResultElement(result)
       {
           document.querySelector('.js-result').innerHTML = result;
+      }
+
+      let intervalId;
+      const autoPlayButton = document.querySelector('.js-auto-play-button');
+
+      function autoPlay()
+      {
+        if(!isAutoPlaying)
+          {
+                                    // arrow function 
+          intervalId = setInterval(() =>
+          {
+            const playerMove = pickComputerMove();
+            playGame(playerMove);
+          },1000);
+          isAutoPlaying = true;
+          autoPlayButton.classList.add('active-auto-play');
+          autoPlayButton.innerHTML = 'Stop Auto Play';
+        }
+        else
+        {
+          clearInterval(intervalId);
+          isAutoPlaying = false;
+          autoPlayButton.classList.remove('active-auto-play');
+          autoPlayButton.innerHTML = 'Auto Play';
+        }
+      }
+
+      function resetScore()
+      {
+        openModalWindow();
+      }
+
+      function openModalWindow()
+      {
+        modal.classList.add('visible');
+
+        document.querySelector('.js-deny-button').addEventListener('click', () =>{
+        closeModalWindow();
+        });
+
+        document.querySelector('.js-confirm-button').addEventListener('click', () =>{
+        score.wins = 0; 
+        score.losses = 0; 
+        score.ties = 0; 
+        localStorage.removeItem('Score'); 
+        updateResultElement('Make a move');
+        updateScoreElement();
+        closeModalWindow();
+        });
+
+      }
+
+      function closeModalWindow()
+      {
+        modal.classList.remove('visible');
       }
